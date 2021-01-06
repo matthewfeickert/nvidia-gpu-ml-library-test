8d135b8d708f97038708d381f762b97c98cbc193
Matthew Feickert <matthew.feickert@cern.ch>
2021-01-06 17:24:18 -0600

    Add better detect for JAX

--- jax_detect_GPU.py
+++ jax_detect_GPU.py
@@ -1,5 +1,11 @@
 from jax.lib import xla_bridge
 
 if __name__ == "__main__":
-    xla_backend = xla_bridge.get_backend().platform  # cpu, gpu, tpu
-    print(f"XLA backend: {xla_backend}")
+    print(f"Number of GPUs found on system: {xla_bridge.device_count()}")
+    xla_backend = xla_bridge.get_backend()
+    xla_backend_type = xla_bridge.get_backend().platform  # cpu, gpu, tpu
+    print(f"XLA backend type: {xla_backend_type}")
+    if xla_backend_type == "gpu":
+        for device in xla_backend.devices():
+            print(f"Active GPU index: {device.id}")
+            print(f"Active GPU name: {device.device_kind}")
