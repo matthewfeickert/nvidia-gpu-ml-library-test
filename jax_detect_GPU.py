48b58d20d2dc91705abdd49f46798da755a5da1e
Matthew Feickert <matthew.feickert@cern.ch>
2021-01-07 01:16:07 -0600

    add space

--- jax_detect_GPU.py
+++ jax_detect_GPU.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     xla_backend = xla_bridge.get_backend()
     xla_backend_type = xla_bridge.get_backend().platform  # cpu, gpu, tpu
     print(f"XLA backend type: {xla_backend_type}")
+
     print(f"\nNumber of GPUs found on system: {xla_bridge.device_count()}")
     if xla_backend_type == "gpu":
         for idx, device in enumerate(xla_backend.devices()):
--- torch_detect_GPU.py
+++ torch_detect_GPU.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     print(f"PyTorch build CUDA version: {torch.version.cuda}")
     print(f"PyTorch build cuDNN version: {torch.backends.cudnn.version()}")
     print(f"PyTorch build NCCL version: {torch.cuda.nccl.version()}")
-    print(f"\nNumber of GPUs found on system: {cuda.device_count()}")
 
+    print(f"\nNumber of GPUs found on system: {cuda.device_count()}")
     is_gpu_available = cuda.is_available()
     print(f"PyTorch has active GPU: {is_gpu_available}")
     if is_gpu_available:
