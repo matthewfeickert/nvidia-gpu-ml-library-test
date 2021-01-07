ef61ec0be41235c74e2f9bdd67cab3b049cdc699
Matthew Feickert <matthew.feickert@cern.ch>
2021-01-07 01:05:22 -0600

    build info first torch

--- torch_detect_GPU.py
+++ torch_detect_GPU.py
@@ -3,13 +3,13 @@ from torch import cuda
 
 if __name__ == "__main__":
     # c.f. https://chrisalbon.com/deep_learning/pytorch/basics/check_if_pytorch_is_using_gpu/
-    print(f"Number of GPUs found on system: {cuda.device_count()}")
+    print(f"PyTorch build CUDA version: {torch.version.cuda}")
+    print(f"PyTorch build cuDNN version: {torch.backends.cudnn.version()}")
+    print(f"PyTorch build NCCL version: {torch.cuda.nccl.version()}")
+    print(f"\nNumber of GPUs found on system: {cuda.device_count()}")
+
     is_gpu_available = cuda.is_available()
     print(f"PyTorch has active GPU: {is_gpu_available}")
     if is_gpu_available:
-        print(f"PyTorch build CUDA version: {torch.version.cuda}")
-        print(f"PyTorch build cuDNN version: {torch.backends.cudnn.version()}")
-        print(f"PyTorch build NCCL version: {torch.cuda.nccl.version()}")
-
         print(f"\nActive GPU index: {cuda.current_device()}")
         print(f"Active GPU name: {cuda.get_device_name(cuda.current_device())}")
