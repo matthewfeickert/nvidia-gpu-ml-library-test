4938e35578ecb0af0a745c1ef0d2432635e499d2
Matthew Feickert <matthew.feickert@cern.ch>
2021-01-07 00:35:31 -0600

    Add CUDA print to TF

--- tf_detect_GPU.py
+++ tf_detect_GPU.py
@@ -8,7 +8,13 @@ if __name__ == "__main__":
 
     print(f"\nTensorFlow build supports GPU: {tf.test.is_built_with_gpu_support()}")
     print(f"TensorFlow build supports XLA: {tf.test.is_built_with_xla()}")
-    print(f"TensorFlow build supports CUDA: {tf.test.is_built_with_cuda()}")
+    is_built_with_cuda = tf.test.is_built_with_cuda()
+    print(f"TensorFlow build supports CUDA: {is_built_with_cuda}")
+
+    if is_built_with_cuda:
+        build_info = tf.sysconfig.get_build_info()
+        print(f"TensorFlow build CUDA version: {build_info['cuda_version']}")
+        print(f"TensorFlow build cuDNN version: {build_info['cudnn_version']}")
 
     for device, details in zip(gpu_devices, gpu_details):
         print(f"\nGPU: {device.name}")
