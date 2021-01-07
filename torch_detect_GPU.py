90e60961d01848d6bf1ed377536d4585029f7c74
Matthew Feickert <matthew.feickert@cern.ch>
2021-01-07 00:45:26 -0600

    Add build info to torch

--- torch_detect_GPU.py
+++ torch_detect_GPU.py
@@ -1,3 +1,4 @@
+import torch
 from torch import cuda
 
 if __name__ == "__main__":
@@ -6,5 +7,8 @@ if __name__ == "__main__":
     is_gpu_available = cuda.is_available()
     print(f"PyTorch has active GPU: {is_gpu_available}")
     if is_gpu_available:
-        print(f"Active GPU index: {cuda.current_device()}")
+        print(f"PyTorch build CUDA version: {torch.version.cuda}")
+        print(f"PyTorch build cuDNN version: {torch.backends.cudnn.version()}")
+        print(f"PyTorch build NCCL version: {torch.cuda.nccl.version()}")
+        print(f"\nActive GPU index: {cuda.current_device()}")
         print(f"Active GPU name: {cuda.get_device_name(cuda.current_device())}")
