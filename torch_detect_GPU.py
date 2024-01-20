551fe6174a6de6096192e733b4946869ed37b0d4
Matthew Feickert <matthew.feickert@cern.ch>
2024-01-20 16:00:36 -0600

    Try to detect metal
    https://developer.apple.com/metal/pytorch/

--- torch_detect_GPU.py
+++ torch_detect_GPU.py
@@ -2,14 +2,20 @@ import torch
 from torch import cuda
 
 if __name__ == "__main__":
-    # c.f. https://chrisalbon.com/deep_learning/pytorch/basics/check_if_pytorch_is_using_gpu/
-    print(f"PyTorch build CUDA version: {torch.version.cuda}")
-    print(f"PyTorch build cuDNN version: {torch.backends.cudnn.version()}")
-    print(f"PyTorch build NCCL version: {torch.cuda.nccl.version()}")
+    if torch.backends.cuda.is_built():
+        # c.f. https://chrisalbon.com/deep_learning/pytorch/basics/check_if_pytorch_is_using_gpu/
+        print(f"PyTorch build CUDA version: {torch.version.cuda}")
+        print(f"PyTorch build cuDNN version: {torch.backends.cudnn.version()}")
+        print(f"PyTorch build NCCL version: {torch.cuda.nccl.version()}")
 
-    print(f"\nNumber of GPUs found on system: {cuda.device_count()}")
-    is_gpu_available = cuda.is_available()
-    print(f"PyTorch has active GPU: {is_gpu_available}")
-    if is_gpu_available:
-        print(f"\nActive GPU index: {cuda.current_device()}")
-        print(f"Active GPU name: {cuda.get_device_name(cuda.current_device())}")
+        print(f"\nNumber of GPUs found on system: {cuda.device_count()}")
+
+        is_gpu_available = cuda.is_available()
+
+        print(f"PyTorch has active GPU: {is_gpu_available}")
+        if is_gpu_available:
+            print(f"\nActive GPU index: {cuda.current_device()}")
+            print(f"Active GPU name: {cuda.get_device_name(cuda.current_device())}")
+    elif torch.backends.mps.is_available():
+        mps_device = torch.device("mps")
+        print(f"PyTorch has active GPU: {mps_device}")
