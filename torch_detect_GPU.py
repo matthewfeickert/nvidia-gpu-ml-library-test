850cc8c601601a691ffd7b0784eab2b42e870400
Matthew Feickert <matthew.feickert@cern.ch>
2024-01-21 00:20:50 -0600

    update fall through

--- torch_detect_GPU.py
+++ torch_detect_GPU.py
@@ -10,12 +10,11 @@ if __name__ == "__main__":
 
         print(f"\nNumber of GPUs found on system: {cuda.device_count()}")
 
-        is_gpu_available = cuda.is_available()
-
-        print(f"PyTorch has active GPU: {is_gpu_available}")
-        if is_gpu_available:
-            print(f"\nActive GPU index: {cuda.current_device()}")
-            print(f"Active GPU name: {cuda.get_device_name(cuda.current_device())}")
+    if cuda.is_available():
+        print(f"\nActive GPU index: {cuda.current_device()}")
+        print(f"Active GPU name: {cuda.get_device_name(cuda.current_device())}")
     elif torch.backends.mps.is_available():
         mps_device = torch.device("mps")
         print(f"PyTorch has active GPU: {mps_device}")
+    else:
+        print(f"PyTorch has no active GPU")
